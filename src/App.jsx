dc59ea02c18d55ab3089bc26c545fec85241946e
Amit Kumar <kumaramit20204@gmail.com>
2025-08-21 16:24:54 +0530

    component-structure-setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
-import { useState } from 'react'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-     hi
+     Hiii
     </>
   )
 }
 
-export default App
+export default App;
